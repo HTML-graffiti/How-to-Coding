b3aa146383061e9e6522808bacd2c3b26f97b522
Very Gois <we.are.pe.hu@gmail.com>
2021-08-15 09:24:54 +0900

    workshop

---
+++ workshop/basic.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+    <title>mouse drag scrollable 2</title>
+    <style type="text/css">
+
+      .library h1,
+      .library u {
+        font-family: "游ゴシック","YuGothic","Yu Gothic","游ゴシック体";
+      }
+      .library b,
+      .library i {
+        font-family: "YuMincho","Yu Mincho","游明朝体";
+      }
+      .library li{
+        background:#fff;
+        font-size:1rem;
+        padding: 2.5vh 1rem;
+        font-family: monospace;
+        color:limegreen;
+      }
+      .library h1 {
+        font-size:200%;
+        font-weight: 500;
+        color:blue;
+      }
+      .library a {
+        background:limegreen;
+        color:blue;
+      }
+      .library hr {
+        border:none;
+        clear:both;
+        padding:0.25rem 0;
+        border-bottom:0.15rem dotted blue;
+      }
+
+    </style>
+
+    <meta
+      name="description"
+      content="https://www.it-the-best.com/entry/javascript-technic-mousedragscroll"
+    />
+
+    <!-- Import the webpage's stylesheet -->
+    <link rel="stylesheet" href="http://creative-community.pe.hu/coding/js/mousedragscrollable/scrollable.css" />
+
+  </head>
+  <body>
+    <div class="library">
+      <b class="left">left</b>
+      <b class="right">right</b>
+      <h1 class="right">H1</h1>
+      <p class="left">p class="left"</p>
+      <hr/>
+      <p><u>u</u></p>
+      <h1><a href="part2.zip">Download</a></h1>
+    </div>
+  </body>
+
+</html>
