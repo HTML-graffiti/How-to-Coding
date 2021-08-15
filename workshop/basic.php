50abdddc751284542330ea539d16600df39b64cb
Very Gois <we.are.pe.hu@gmail.com>
2021-08-15 09:37:44 +0900

    Update basic.php

--- workshop/basic.php
+++ workshop/basic.php
@@ -9,43 +9,14 @@
     <title>mouse drag scrollable 2</title>
     <style type="text/css">
 
-      .schedule h1,
-      .schedule u {
-        font-family: "游ゴシック","YuGothic","Yu Gothic","游ゴシック体";
-      }
-      .schedule b,
-      .schedule i {
-        font-family: "YuMincho","Yu Mincho","游明朝体";
-      }
-      .schedule h1 {
-        font-size:200%;
-        font-weight: 500;
-        color:blue;
-      }
-      .schedule a {
-        background:limegreen;
-        color:blue;
-      }
-
-      .schedule hr {
-        clear:both;
-        padding:0.25rem 0;
-        border-bottom:0.15rem dotted blue;
-      }
-
     </style>
 
-    <meta
-      name="description"
-      content="https://www.it-the-best.com/entry/javascript-technic-mousedragscroll"
-    />
-
     <!-- Import the webpage's stylesheet -->
-    <link rel="stylesheet" href="http://creative-community.pe.hu/coding/js/mousedragscrollable/scrollable.css" />
+    <link rel="stylesheet" href="" />
 
   </head>
   <body>
-    <div class="library schedule">
+    <div class="">
       <b class="left">left</b>
       <b class="right">right</b>
       <h1 class="right">H1</h1>
