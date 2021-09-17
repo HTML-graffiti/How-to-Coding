84c234ca39a66822d148235be3730c747682b5ee
∧° ┐ <we.are.pe.hu@gmail.com>
2021-09-17 11:10:23 +0900

    text

--- update/index.php
+++ update/index.php
@@ -28,6 +28,8 @@ fclose($fp);
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="description" content="これまでに制作したウェブページ、コーディングのヒント・便利なツールなどをリストアップします。">
+
 <title> Update | How to Coding </title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="/coding/submit/org/org.js"></script>
