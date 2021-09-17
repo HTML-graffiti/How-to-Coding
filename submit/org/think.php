d5f9f40e4895754043f268bbecf18cf7c6885686
∧° ┐ <we.are.pe.hu@gmail.com>
2021-09-17 12:11:58 +0900

    org

--- submit/org/think.php
+++ submit/org/think.php
@@ -27,7 +27,7 @@ fclose($fp);
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title> ORG | Think Book </title>
+<title> ORG | How to Coding </title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="org.js"></script>
 <script type="text/javascript">
