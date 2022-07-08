c5d7131f21b8d7cee183177807a619d037b85f4f
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-08 18:03:33 +0900

    js

--- js/tone/scrool/scrool.js
+++ js/tone/scrool/scrool.js
@@ -4,33 +4,30 @@ var lastScrollTop = 0;
 var reverse = false;
 
 var scrool = new Tone.AMSynth().toMaster();
-var autoFilter = new Tone.AutoFilter({
-}).connect(Tone.Master);
+var autoFilter = new Tone.AutoFilter({}).connect(Tone.Master);
 
 autoFilter.start()
 StartAudioContext(Tone.context, 'div');
 //have to click to start audio context
-$('div').click(function(){
-  Tone.start();
+$('div').click(function() {
+    Tone.start();
 });
 
-$(window).scroll(function(event){
-  var st = $(this).scrollTop();
-  console.log(st);
-  if (st > lastScrollTop){
-  //scrolled down
-scrool.triggerAttackRelease("C4", 2);
-  } else {
-  //scrolled up
-    scrool.triggerAttackRelease("G4", 2);
-  }
-  lastScrollTop = st;
+$(window).scroll(function(event) {
+    var st = $(this).scrollTop();
+    console.log(st);
+    if (st > lastScrollTop) {
+        //scrolled down
+        scrool.triggerAttackRelease("C4", 2);
+    } else {
+        //scrolled up
+        scrool.triggerAttackRelease("G4", 2);
+    }
+    lastScrollTop = st;
 
 });
 
-
-
-$(window).resize(function(){
-  w = $(window).width();
-  h = $(window).height();
+$(window).resize(function() {
+    w = $(window).width();
+    h = $(window).height();
 });
