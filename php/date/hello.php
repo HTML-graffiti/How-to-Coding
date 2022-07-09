ad9690bf22acf6213e1ba8e6aa7ede86774ed818
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-09 18:40:04 +0900

    Update hello.php

--- php/date/hello.php
+++ php/date/hello.php
@@ -42,6 +42,25 @@
         <br/>c: ISO8601 フォーマット日付
         <br/>r: RFC2822 フォーマット日付
     </p>
+    <p>
+        <sup>echo date("全日付")</sup>
+        <br/>
+        <b>
+        <?php
+        date_default_timezone_set('Asia/Tokyo');
+        echo date("d D");
+        ?>
+        <br/>
+        <?php
+        date_default_timezone_set('Asia/Tokyo');
+        echo date("j l");
+        ?>
+      </b>
+        <br/>d: 日。2桁の数字(ゼロ詰め)
+        <br/>D: 曜日。3文字のテキスト形式
+        <br/>j: 日。(1桁はゼロ無し)
+        <br/>l: 曜日。フルスペル形式
+    </p>
 </body>
 
 </html>
