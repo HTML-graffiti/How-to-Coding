1ffcf5ff1829ceb87185d3094259a7d7d4a90ff0
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-09 19:46:26 +0900

    Update date_sun_info.php

--- php/date/date_sun_info.php
+++ php/date/date_sun_info.php
@@ -16,30 +16,29 @@
         <br/>
         <b>
         <?php
-        $timeZone = 'Asia/Tokyo';
-
-        $period = new DatePeriod(
-          date_create('first day of this month', timezone_open($timeZone)),
-          new DateInterval('P1D'),
-          date_create('last day of this month +1 second', timezone_open($timeZone))
-        );
-
-        $location = timezone_open($timeZone)->getLocation();
-        printf('%10s%9s%8s' . PHP_EOL, 'date', 'sunrise', 'sunset');
-
-        foreach ($period as $day) {
-          $sunInfo = date_sun_info(
-            $day->getTimeStamp(),
-            $location['latitude'],
-            $location['longitude']
-          );
-    
-          printf('%s%9s%8s' . PHP_EOL,
-          $day->format('Y-m-d'),
-          $day->setTimeStamp($sunInfo['sunrise'])->format('H:i'),
-          $day->setTimeStamp($sunInfo['sunset'])->format('H:i')
-        );
-      }
+ 
+ $lon = 34.6165;
+ $lat = 135.4855;
+  
+ echo date("D M d Y"). ', sunrise time : ' . date_sunrise(
+     time(),
+     SUNFUNCS_RET_STRING,
+     $lat,
+     $lon,
+     90,
+     9
+ );
+  
+ echo "<br>";
+  
+ echo date("D M d Y"). ', sunset time : ' . date_sunset(
+     time(),
+     SUNFUNCS_RET_STRING,
+     $lat,
+     $lon,
+     90,
+     9
+ );
         ?>
       </b>
     </p>
