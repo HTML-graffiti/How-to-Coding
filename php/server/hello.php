71bd4fe28d42f63f9325b27c22823414b4962b3c
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-09 21:43:23 +0900

    Update hello.php

--- php/server/hello.php
+++ php/server/hello.php
@@ -13,8 +13,8 @@
 <body>
 <p class="ip">
 <?php
-echo 'USER : '. $_SERVER['HTTP_USER_AGENT']."<br/>";
 echo 'IP : '. $_SERVER['REMOTE_ADDR']."<br/>";
+echo 'USER : '. $_SERVER['HTTP_USER_AGENT']."<br/>";
 echo 'SERVER : '. $_SERVER['SERVER_NAME']."<br/>";
 echo 'PROTOCOL : '. $_SERVER['SERVER_PROTOCOL']."<br/>";
 ?>
