04d449090dd557517830c99b7bff686f8b39d868
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-09 18:48:55 +0900

    Update hello.php

--- php/date/hello.php
+++ php/date/hello.php
@@ -61,6 +61,48 @@
         <br/>j: 日。(1桁はゼロ無し)
         <br/>l: 曜日。フルスペル形式
     </p>
+    <p>
+        <sup>echo date("月 | 年")</sup>
+        <br/>
+        <b>
+        <?php
+        date_default_timezone_set('Asia/Tokyo');
+        echo date("F n Y");
+        ?>
+        <br/>
+        <?php
+        date_default_timezone_set('Asia/Tokyo');
+        echo date("M m y");
+        ?>
+      </b>
+        <br/>F: 月。フルスペルの文字
+        <br/>n: 月。数字(1桁はゼロ無し)
+        <br/>M: 月。3文字形式
+        <br/>m: 月。数字(ゼロ詰め)
+        <br/>Y: 年。4 桁の数字
+        <br/>y: 年。2 桁の数字
+    </p>
+    <p>
+        <sup>echo date("時 | 分")</sup>
+        <br/>
+        <b>
+        <?php
+        date_default_timezone_set('Asia/Tokyo');
+        echo date("a g i s");
+        ?>
+        <br/>
+        <?php
+        date_default_timezone_set('Asia/Tokyo');
+        echo date("A h i s");
+        ?>
+      </b>
+        <br/>a: 午前または午後（小文字）
+        <br/>A: 午前または午後（大文字）
+        <br/>g: 時。12時間単位(1桁はゼロ無し)
+        <br/>h: 時。数字。12 時間単位(ゼロ詰め)
+        <br/>i: 分(ゼロ詰め)
+        <br/>s: 秒(ゼロ詰め)
+    </p>
 </body>
 
 </html>
