e222b185df60c1dc8aa6f4e2c39718288e04c126
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-09 19:50:39 +0900

    Update date_sun_info.php

--- php/date/date_sun_info.php
+++ php/date/date_sun_info.php
@@ -25,10 +25,9 @@ $longitude = 135.4855;
 echo 'sunrise: ' . date_sunrise($now, SUNFUNCS_RET_STRING, $latitude, $longitude) . PHP_EOL;
 echo 'sunset: ' . date_sunset($now, SUNFUNCS_RET_STRING, $latitude, $longitude) . PHP_EOL;
 
-echo str_repeat('-', 40) . PHP_EOL;
 $sun_info = date_sun_info($now, $latitude, $longitude);
 foreach ($sun_info as $key => $val) {
-    echo "$key: " . date('H:i:s', $val) . PHP_EOL;
+    echo "$key: " . date('H:i:s', $val) . "\n";
 }
         ?>
       </b>
