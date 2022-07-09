78e5c75515114c66e0a91b082e481c01dfd36023
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-09 19:17:23 +0900

    Update date_sun_info.php

--- php/date/date_sun_info.php
+++ php/date/date_sun_info.php
@@ -16,7 +16,7 @@
         <br/>
         <b>
         <?php
-        $sun_info = date_sun_info(strtotime("2006-12-12"), 34.616575, 135.485586);
+        $sun_info = date_sun_info(strtotime("Y-m-d"), 34.616575, 135.485586);
         foreach ($sun_info as $key => $val) {
           echo "$key: " . date("H:i:s", $val) . "\n";
         }
