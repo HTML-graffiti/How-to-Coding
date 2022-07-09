2e603a28919232b618269dad67273e281b0302d1
∧° ┐ <we.are.pe.hu@gmail.com>
2022-07-09 20:31:30 +0900

    Update date_sun_info.php

--- php/date/date_sun_info.php
+++ php/date/date_sun_info.php
@@ -23,7 +23,10 @@ $longitude = 135.4855;
 
 $sun_info = date_sun_info($today, $latitude, $longitude);
 foreach ($sun_info as $key => $val) {
-    echo "$key: " . date('g:i:s A', $val) . " | \n";
+  
+  echo "$sunrise 日の出	| " . date('g:i:s A', $val) . " | \n";
+  echo "$transit 日南中時	| " . date('g:i:s A', $val) . " | \n";
+  echo "$sunset 日の入り	| " . date('g:i:s A', $val) . " | \n";
 }
         ?>
       </b>
