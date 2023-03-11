fd96642e9697678e33c80f5aa6a8d0b59fd4ea39
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-03-11 14:08:29 +0900

    coding

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ const heading = document.createElement("h1");
 heading.setAttribute("id", "title");
 document.body.appendChild(heading);
 
-let left = document.querySelector('#left');
-let right = document.querySelector('#right');
-
 document.addEventListener('readystatechange', (event) => {
-  if (event.target.readyState === 'interactive') {
+  if (event.target.readyState === 'loading') {
+    let bodyAll = document.querySelectorAll('#left, #right');
+    for (const bodyCover of bodyAll) {
+      bodyCover.style.display = "none";
+    }
+  }
+
+  else if (event.target.readyState === 'interactive') {
     heading.innerText = "Now Loding | creative-community.space";
+    let bodyAll = document.querySelectorAll('#left, #right');
+    for (const bodyCover of bodyAll) {
+      bodyCover.style.display = "block";
+      bodyCover.style.opacity = "0";
+    }
   }
 
   else if (event.target.readyState === 'complete') {
     heading.innerText = "How to Coding | creative-community.space";
-
-    let hello = document.createElement('section');
-    hello.setAttribute('id', 'hello');
-    hello.classList.add('data_show')
-    left.prepend(hello);
-
-    let br = document.createElement('br');
-    hello.after(br);
-
-    $(function() {
-      $("#hello").load("hello.html");
-    });
+    let bodyAll = document.querySelectorAll('#left, #right');
+    for (const bodyCover of bodyAll) {
+      bodyCover.style.opacity = "1";
+    }
   }
 });
