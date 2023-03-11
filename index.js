9618a9e8a4a8b163ce661320cdf58dadce51cace
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-03-11 14:06:59 +0900

    coding

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ const heading = document.createElement("h1");
 heading.setAttribute("id", "title");
 document.body.appendChild(heading);
 
-let bodyAll = document.querySelectorAll('#left, #right');
+let left = document.querySelector('#left');
+let right = document.querySelector('#right');
 
 document.addEventListener('readystatechange', (event) => {
-  if (event.target.readyState === 'loading') {
-    for (const bodyCover of bodyAll) {
-      bodyCover.style.display = "none";
-    }
-  }
-
-  else if (event.target.readyState === 'interactive') {
+  if (event.target.readyState === 'interactive') {
     heading.innerText = "Now Loding | creative-community.space";
-    for (const bodyCover of bodyAll) {
-      bodyCover.style.display = "block";
-      bodyCover.style.opacity = "0";
-    }
   }
 
   else if (event.target.readyState === 'complete') {
     heading.innerText = "How to Coding | creative-community.space";
-    for (const bodyCover of bodyAll) {
-      bodyCover.style.opacity = "1";
-    }
+
+    let hello = document.createElement('section');
+    hello.setAttribute('id', 'hello');
+    hello.classList.add('data_show')
+    left.prepend(hello);
+
+    let br = document.createElement('br');
+    hello.after(br);
+
+    $(function() {
+      $("#hello").load("hello.html");
+    });
   }
 });
