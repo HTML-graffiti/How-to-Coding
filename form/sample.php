7247748a4b7d5d66071fe72e0df94bd925d102ba
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-03-16 19:26:40 +0900

    coding

--- form/sample.php
+++ form/sample.php
@@ -4,8 +4,8 @@
 $name = htmlspecialchars($_POST['name']);
 $email  = htmlspecialchars($_POST['email']);
 $website  = htmlspecialchars($_POST['website']);
+$phone  = htmlspecialchars($_POST['phone']);
 $message  = htmlspecialchars($_POST['message']);
-$pass  = htmlspecialchars($_POST['pass']);
 
 ?>
 
@@ -15,19 +15,17 @@ $pass  = htmlspecialchars($_POST['pass']);
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width">
   <meta name="format-detection" content="telephone=no" />
+  <link rel="stylesheet" href="index.css">
   <style type="text/css">
   @font-face {
     font-family: "ipag";
     src: url("https://creative-community.space/ver/ipag.ttf");
   }
 
-  body {
-    font-family: 'Arial Narrow', 'Arial', sans-serif;
-  }
-
   main {
+    font-family: 'Arial Narrow', 'Arial', sans-serif;
     font-size: 1rem;
-    margin: 2.5rem auto 7.5rem;
+    margin: 2.5rem auto 5rem;
     max-width: 500px;
     width: 75%;
   }
@@ -38,19 +36,22 @@ $pass  = htmlspecialchars($_POST['pass']);
     word-break: break-all;
   }
 
-  h1,
+  h1 {
+    padding: 0;
+    margin: 0.5rem 0 1rem;
+  }
+
   h2,
   h3 {
     padding: 0.25rem;
     margin: 0;
   }
 
-  h1 {
+  h2 {
     font-size: 200%;
     font-weight: 200;
   }
 
-  h2,
   h3,
   a,
   u {
@@ -73,22 +74,65 @@ $pass  = htmlspecialchars($_POST['pass']);
     width: 100%;
     overflow: auto;
   }
+
+  details {
+    margin: auto;
+    max-width: 750px;
+  }
+
+  summary {
+    display: block;
+    position: relative;
+    width: 100%;
+  }
+
+  summary::marker,
+  summary::-webkit-details-marker {
+    display: none;
+  }
+
+  summary::before {
+    content: "→";
+    display: inline-block;
+    background-color: #000;
+    border-radius: 0.5rem;
+    color: #fff;
+    font-size: 125%;
+    padding: 0.25rem 0.5rem;
+    margin: 0.25rem 0.5rem 0.25rem 0;
+    transition: 0.5s;
+  }
+
+  /* オープン時 */
+  details[open] summary::before {
+    transform: rotate(90deg); /* 90度回転 */
+  }
   </style>
 </head>
 <body>
+  <h1>Web Form | Retrieving the data by PHP</h1>
   <main>
-  <h1>I am <strong><?php echo $name; ?></strong></h1>
-  <h2>Email: <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></h2>
-  <h3>Website: <a href="<?php echo $website; ?>" target="_blank" rel="noreferrer"><?php echo $website; ?></a></h3>
-  <br/>
-  <h3>Phone: <u><?php echo $pass; ?></u></h3>
-  <hr/>
-  <p><?php echo $message; ?></p>
-</main>
-  <pre>
+    <h2>I am <strong><?php echo $name ?></strong></h2>
+    <h3>Email: <a href="mailto:<?php echo $email ?>"><?php echo $email ?></a></h3>
+    <h3>Website: <a href="<?php echo $website ?>" target="_blank" rel="noreferrer"><?php echo $website ?></a></h3>
+    <br/>
+    <h3>Phone: <u><?php echo $phone ?></u></h3>
+    <hr/>
+    <p><?php echo $message ?></p>
+  </main>
+  <details>
+    <summary>フォームのデータを PHP で取得する</summary>
+    <p>
+      $_POST グローバル変数は、POST メソッドで送信されたデータへ名前でアクセスします。<br/>
+      <small>GET メソッドで送信されたデータにアクセスするには、$_GET が使用できます。</small>
+    </p>
+    <p>
+      <i>$変数名 = 値</i> — <small>値 を 変数 に代入する</small><br/>
+      <i>htmlspecialchars</i>( ) — <small>特殊文字を HTML エンティティに変換する</small><br/>
+      <i>$_POST</i>['コントロール名'] - <small>フォームコントロールの name 属性に設定した値を記述する</small>
+    </p>
+    <pre>
 <<code>?php</code>
-// $_POST グローバル変数は、POST メソッドで送信されたデータへ名前でアクセスします。
-// GET メソッドで送信されたデータにアクセスするには、$_GET が使用できます。
 
 $name = <code>htmlspecialchars</code>(<code>$_POST['name']</code>);
 $email  = <code>htmlspecialchars</code>(<code>$_POST['email']</code>);
@@ -97,13 +141,21 @@ $phone  = <code>htmlspecialchars</code>(<code>$_POST['phone']</code>);
 $message  = <code>htmlspecialchars</code>(<code>$_POST['message']</code>);
 
 <code>?</code>>
-  </pre>
-  <pre>
-<<b>?php</b> <code>echo $name</code>; <b>?</b>> // input name="name" の値を出力
-<<b>?php</b> <code>echo $email</code>; <b>?</b>> // input name="email" の値を出力
-<<b>?php</b> <code>echo $website</code>; <b>?</b>> // input name="website" の値を出力
-<<b>?php</b> <code>echo $phone</code>; <b>?</b>> // input name="phone" の値を出力
-<<b>?php</b> <code>echo $message</code>; <b>?</b>> // textarea name="message" の値を出力
-  </pre>
+    </pre>
+  </details>
+  <details>
+    <summary>PHP で 変数の値を出力する</summary>
+    <p>
+      <i>echo</i> — <small>文字列を出力する</small><br/>
+      echo <i>$変数名</i> — <small>変数に代入した値を出力する</small><br/>
+    </p>
+    <pre>
+<<b>?php</b> <code>echo $name</code> <b>?</b>> // input name="name" の値を出力
+<<b>?php</b> <code>echo $email</code> <b>?</b>> // input name="email" の値を出力
+<<b>?php</b> <code>echo $website</code> <b>?</b>> // input name="website" の値を出力
+<<b>?php</b> <code>echo $phone</code> <b>?</b>> // input name="phone" の値を出力
+<<b>?php</b> <code>echo $message</code> <b>?</b>> // textarea name="message" の値を出力
+    </pre>
+  </details>
 </body>
 </html>
