a4b83d43edb880ea2e3fcc7927aeb96274d3e60e
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-03-22 18:53:26 +0900

    coding

--- js/web-storage/script.js
+++ js/web-storage/script.js
@@ -23,41 +23,8 @@ function getStyle() {
   html.style.fontSize = currentSize;
 }
 
-// localStorageへの格納
-function setStorage() {
-  let key = document.querySelector("#textKey").value;
-  let value = document.querySelector("#textValue").value;
-
-  // 値の入力チェック
-  if (key && value) {
-    storage.setItem(key, value);
-  }
-
-  // 変数の初期化
-  key = "";
-  value = "";
-
-  getStorage();
-}
-
-
-function getStorage() {
-  const list = document.getElementById("list")
-  // sessionStorageすべての情報の取得
-  for (var i=0; i < storage.length; i++) {
-    var itemKey = storage.key(i);
-
-    // sessionStorageのキーと値を表示
-    var listP = document.createElement("p");
-    var listKey = document.createElement("small");
-    var listValue = document.createElement("u");
-    listKey.innerHTML = itemKey;
-    listValue.innerHTML = storage.getItem(itemKey);
-    list.appendChild(listP);
-    listP.appendChild(listKey);
-    listP.appendChild(listValue);
-  }
-
+function removeallStorage() {
+  storage.clear();
 }
 
 fontSize.onchange = populateStorage;
