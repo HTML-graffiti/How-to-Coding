d9f87ed0b97330783576e2a0c59ccb34b29db097
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-16 16:47:14 +0900

    Update size.js

--- js/element/size.js
+++ js/element/size.js
@@ -20,7 +20,7 @@ function pageResize() {
   BodySize();
 }
 
-let clientRect = document.querySelector("#rect")
+let clientRect = document.querySelector(".readme")
 
 let rect = clientRect.getBoundingClientRect();
 for (var key in rect) {
@@ -32,7 +32,7 @@ for (var key in rect) {
 }
 
 function HTMLsize() {
-  const readme = document.querySelector("#rect")
+  const readme = document.querySelector(".readme")
 
   const outoffsetWidth = document.getElementById("outoffsetWidth")
   outoffsetWidth.innerText = readme.offsetWidth;
