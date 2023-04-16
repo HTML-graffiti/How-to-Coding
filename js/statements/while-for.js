110a3627046f4280ce63a44befe4be82634aa48d
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-16 17:14:43 +0900

    cc

--- js/statements/while-for.js
+++ js/statements/while-for.js
@@ -13,33 +13,3 @@ for (let color of colors) {
 }
 
 listArr.style.background = '#fff';
-
-
-function documentInfo() {
-  let objectDiv = document.querySelector('div#object');
-
-  let documentInfo = {
-    'URL: ': document.location,
-    'LastModified: ': document.lastModified,
-    'Referrer: ': document.referrer
-  };
-
-  for (let property in documentInfo) {
-    objectDiv.innerHTML += (`<p>${property} <code>${documentInfo[property]}</code></p>`);
-  }
-}
-
-function viewColors() {
-  let okCancel = confirm('このページの情報を表示する')
-
-  switch (okCancel) {
-    case true:
-    documentInfo()
-
-    let helloButton = document.querySelector('#hello button');
-    helloButton.remove()
-    break;
-
-    default:
-  }
-}
