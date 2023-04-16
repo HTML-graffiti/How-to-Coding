86e1a3225ea2b14f952e370fc248de6e801e73f8
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-16 16:10:01 +0900

    cc

--- readme/script.js
+++ readme/script.js
@@ -5,14 +5,12 @@ const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
 
 anchorLinks.forEach(link => {
   link.addEventListener('click', event => {
-      event.preventDefault();
-
+    event.preventDefault();
     const readmeSectionAll = document.querySelectorAll(".readme section");
     for (const readmeSection of readmeSectionAll) {
       readmeSection.classList.remove("active");
       readmeSection.classList.add("is-hide");
     }
-
     const targetId = link.hash;
     const targetElement = document.querySelector(targetId);
     targetElement.classList.replace("is-hide", "active");
