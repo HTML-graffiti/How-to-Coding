3ff8510d4ee8f26486039605453a6ebfb9c03f16
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-04-29 11:47:34 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no" />
-
-  <script src="index.js" async></script>
   <script src="readme/index.js" async></script>
-
-  <link rel="icon" href="/ver/logo.png">
-  <link rel="apple-touch-icon" href="/ver/logo.png">
+  <script src="index.js" async></script>
   <link rel="stylesheet" href="readme/style.css">
   <link rel="stylesheet" href="cover/style.css">
   <link rel="stylesheet" href="cover/mobile.css" media="screen and (max-width: 820px)">
+  <link rel="icon" href="/profile/icon.png">
+  <link rel="icon" href="/profile/android.png" sizes="192x192" type="image/png">
+  <link rel="apple-touch-icon-precomposed" href="/profile/apple.png" sizes="180x180" type="image/png">
 </head>
 
 <body id="cover" ononline="update(true)" onoffline="update(false)" onload="update(navigator.onLine)">
